883ca51451b5a0ad4e0c48eb80024b6ce9e5ca1e
Arushi Tandon <135385191+ArushiTandon@users.noreply.github.com>
2024-09-24 18:21:23 +0530

    Task2 updated

--- play.js
+++ play.js
@@ -1,10 +1,17 @@
-const sum = (a , b) => a + b;
+//product of 2 nums.
+const sum = (a , b) => a * b;
 
-console.log(sum(1,2));
+console.log(sum(5,2));
 
+//student object.
 const student = {
     name: 'Arushi',
     age: '24',
-    roll_no: '1'
+    roll_no: '1',
+    greet(){
+        console.log('Hello, I am ' + this.name);
+    }
 }
+
+student.greet();
 console.log(student);

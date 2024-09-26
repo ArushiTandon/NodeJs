3328a6e068e41670bd47f0fdb2be46e03b756e61
Arushi Tandon <135385191+ArushiTandon@users.noreply.github.com>
2024-09-26 21:55:23 +0530

    Request/Response

--- app.js
+++ app.js
@@ -1,8 +1,29 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    console.log('Arushi');
-    
+   const url = req.url;
+   if(url === '/home'){
+    res.write('<html>');
+    res.write('<head><title>Welcome</title></head>');
+    res.write('<body><h1>Welcome Home</h1></body>');
+    res.write('</html>');
+    return res.end();
+   } 
+   if(url === '/about'){
+    res.write('<html>');
+    res.write('<head><title>Welcome</title></head>');
+    res.write('<body><h1>Welcome to About Us page</h1></body>');
+    res.write('</html>');
+    return res.end();
+   } 
+   if(url === '/node'){
+    res.write('<html>');
+    res.write('<head><title>Welcome</title></head>');
+    res.write('<body><h1>Welcome to my Node Js project</h1></body>');
+    res.write('</html>');
+    return res.end();
+   }
+   
 });
 
 server.listen(4000);

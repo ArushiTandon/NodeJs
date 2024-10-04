5e866fec5ba8bd527bb042b4f24722e44c78ec1e
Arushi Tandon <135385191+ArushiTandon@users.noreply.github.com>
2024-10-04 19:28:18 +0530

    Updated code inc. 404 page controller

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const app = express();
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+const errorController = require('./controllers/error');
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
@@ -17,7 +18,6 @@ app.get('/', (req, res, next) => {
     res.redirect('/admin/add-product');
 });
 
-app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
-})
+app.use(errorController.geterror);
+
 app.listen(4000);
